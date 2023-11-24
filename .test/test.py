61511a157f6c5f47789b3b27304f2e287c1977c4
Muhammad Ali Arshad <aliarshadciitswl@gmail.com>
2023-11-24 13:51:23 +0800

    Create test.py

---
+++ .test/test.py
@@ -0,0 +1,8 @@
+# tests/test_model.py
+
+import your_model_file  # Import the file where your model is defined
+import pytest
+
+def test_model_accuracy():
+    # Write a test to check the accuracy of your model
+    assert your_model_file.accuracy > 0.9  # Replace with your accuracy metric
